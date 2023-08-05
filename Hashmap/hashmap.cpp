0839c554627f52a595c9f3ca37818206a2d2003d
Mansvi Kmable <mansvikamble27@gmail.com>
2023-08-06 01:06:09 +0530

    New

--- Hashmap/hashmap.cpp
+++ Hashmap/hashmap.cpp
@@ -1,3 +1,19 @@
-#include<iostream>
+#include <iostream>
 #include<map>
 #include<unordered_map>
+using namespace std;
+
+int main()
+{
+    unordered_map<string,int> m;
+    pair<string,int> p = make_pair("Mansvi",22);
+    m.insert(p);
+    
+    pair<string,int> pair1("Vivekanand",19);
+    
+    m["kamble"] = 3;
+    
+    cout<<m["Kamble"]<<endl;
+
+    return 0;
+}
